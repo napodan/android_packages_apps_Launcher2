cdd4b0f805497f6a747b0266460afa09258b31b6
Adam Green <greena88@gmail.com>
2011-11-13 14:37:51 +0000

    Added endless looping, added preference controls, made menu dock similar to Zeam layout.

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -108,11 +108,11 @@ public final class Launcher extends Activity
     private static final int MENU_GROUP_WALLPAPER = MENU_GROUP_ADD + 1;
 
     private static final int MENU_ADD = Menu.FIRST + 1;
-    private static final int MENU_MANAGE_APPS = MENU_ADD + 1;
-    private static final int MENU_WALLPAPER_SETTINGS = MENU_MANAGE_APPS + 1;
+    private static final int MENU_WALLPAPER_SETTINGS = MENU_ADD + 1;
     private static final int MENU_SEARCH = MENU_WALLPAPER_SETTINGS + 1;
-    private static final int MENU_NOTIFICATIONS = MENU_SEARCH + 1;
-    private static final int MENU_SETTINGS = MENU_NOTIFICATIONS + 1;
+    private static final int MENU_APPS = MENU_SEARCH + 1;
+    private static final int MENU_LAUNCHER_SETTINGS = MENU_APPS + 1;
+    private static final int MENU_SETTINGS = MENU_LAUNCHER_SETTINGS + 1;
 
     private static final int REQUEST_CREATE_SHORTCUT = 1;
     private static final int REQUEST_CREATE_LIVE_FOLDER = 4;
@@ -1131,18 +1131,18 @@ public final class Launcher extends Activity
         menu.add(MENU_GROUP_ADD, MENU_ADD, 0, R.string.menu_add)
                 .setIcon(android.R.drawable.ic_menu_add)
                 .setAlphabeticShortcut('A');
-        menu.add(0, MENU_MANAGE_APPS, 0, R.string.menu_manage_apps)
-                .setIcon(android.R.drawable.ic_menu_manage)
-                .setAlphabeticShortcut('M');
         menu.add(MENU_GROUP_WALLPAPER, MENU_WALLPAPER_SETTINGS, 0, R.string.menu_wallpaper)
                  .setIcon(android.R.drawable.ic_menu_gallery)
                  .setAlphabeticShortcut('W');
         menu.add(0, MENU_SEARCH, 0, R.string.menu_search)
                 .setIcon(android.R.drawable.ic_search_category_default)
                 .setAlphabeticShortcut(SearchManager.MENU_KEY);
-        menu.add(0, MENU_NOTIFICATIONS, 0, R.string.menu_notifications)
-                .setIcon(com.android.internal.R.drawable.ic_menu_notifications)
-                .setAlphabeticShortcut('N');
+        menu.add(0, MENU_APPS, 0, R.string.group_applications)
+                .setIcon(android.R.drawable.ic_menu_view)
+                .setAlphabeticShortcut('P');
+        menu.add(0, MENU_LAUNCHER_SETTINGS, 0, R.string.menu_launcher_settings)
+                .setIcon(android.R.drawable.ic_menu_manage)
+                .setAlphabeticShortcut('L');
 
         final Intent settings = new Intent(android.provider.Settings.ACTION_SETTINGS);
         settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
@@ -1182,20 +1182,20 @@ public final class Launcher extends Activity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case MENU_LAUNCHER_SETTINGS:
+                showLauncherSettings();
+                return true;
             case MENU_ADD:
                 addItems();
                 return true;
-            case MENU_MANAGE_APPS:
-                manageApps();
+            case MENU_APPS:
+                showAllApps(true);
                 return true;
             case MENU_WALLPAPER_SETTINGS:
                 startWallpaper();
                 return true;
             case MENU_SEARCH:
                 onSearchRequested();
-                return true;
-            case MENU_NOTIFICATIONS:
-                showNotifications();
                 return true;
         }
 
@@ -1232,10 +1232,6 @@ public final class Launcher extends Activity
         int appWidgetId = getAppWidgetHost().allocateAppWidgetId();
         AppWidgetManager.getInstance(this).bindAppWidgetId(appWidgetId, appWidgetProvider);
         addAppWidgetImpl(appWidgetId);
-    }
-
-    private void manageApps() {
-        startActivity(new Intent(android.provider.Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS));
     }
 
     void addAppWidgetFromPick(Intent data) {
@@ -1402,6 +1398,11 @@ public final class Launcher extends Activity
         if (statusBar != null) {
             statusBar.expand();
         }
+    }
+
+    private void showLauncherSettings() {
+        Intent launchPreferencesIntent = new Intent().setClass(this, SettingsActivity.class);
+        startActivity(launchPreferencesIntent);
     }
 
     private void startWallpaper() {
